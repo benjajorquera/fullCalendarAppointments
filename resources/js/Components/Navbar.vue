759064bad7dccefb959429b46d5c05dca2571cf0
Benja Jorquera <benja.jorquera.j@gmail.com>
2022-05-20 05:16:49 -0400

    navbar fix

---
+++ resources/js/Components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav class="navbar sticky-top navbar-dark bg-dark">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#"
+                    >Calendar Appointments Display</a
+                >
+            </div>
+            <ul class="nav navbar-nav">
+                <li class="active">
+                    <a
+                        href="https://github.com/benjajorquera/fullCalendarAppointments"
+                        >Repo</a
+                    >
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {};
+</script>
